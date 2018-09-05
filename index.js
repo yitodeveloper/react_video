caad02bfab1bb76e18a71f6a0b2aa5929db922df
Rodrigo Ramírez <yitodeveloper@gmail.com>
2018-09-05 15:10:33 -0300

    Uso de listas

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import React from 'react' // Permite crear componentes
 import {render} from 'react-dom' // Permite agregar los componentes en algún lugar
-import Media from './src/playlist/components/media'
+import Playlist from './src/playlist/components/playlist'
+import data from './src/api.json'
 
 const app = document.getElementById('app') // Se trae el elemento donde se va a renderizar
 
@@ -8,10 +9,5 @@ const app = document.getElementById('app') // Se trae el elemento donde se va a 
 // ReactDOM.render(Que se va a renderizar, Donde se va a realizar)
 // const holaMundo = <h1>Hola a todo el mundo</h1>
 render(
-    <Media
-        title="¿Que es responsive design?"  
-        author="Rodrigo Ramírez"
-        image="./images/covers/responsive.jpg"
-        type="video"
-     />
+    <Playlist data={data}/>
 ,app)
--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -33,14 +33,14 @@ class Media extends Component {
             <div className="Media" onClick={this.handleClick}>
                 <div className="Media-cover" >
                     <img
-                        src={this.props.image}
+                        src={this.props.cover}
                         alt=""
-                        width={260}
+                        width={240}
                         height={160}
                         className = "Media-image"
                     />
                     <h3 className="Media-title">{this.props.title}</h3>
-                    <p className="Media-author">{this.state.author}</p>
+                    <p className="Media-author">{this.props.author}</p>
                 </div>
             </div>
         )
@@ -48,7 +48,7 @@ class Media extends Component {
 }
 
 Media.propTypes = {
-    images: PropTypes.string,
+    cover: PropTypes.string,
     title: PropTypes.string.isRequired,
     author: PropTypes.string,
     type: PropTypes.oneOf(['video'],['audio'])
