ce4b9132aec4e88368fbe14467e3e76e93272746
Rodrigo Ramírez <yitodeveloper@gmail.com>
2018-09-05 16:46:36 -0300

    Construccón de estructura del proyecto (layout)

--- src/entries/home.js
+++ src/entries/home.js
@@ -1,7 +1,7 @@
 import React from 'react' // Permite crear componentes
 import {render} from 'react-dom' // Permite agregar los componentes en algún lugar
-import Playlist from './src/playlist/components/playlist'
-import data from './src/api.json'
+import Home from '../pages/containers/home'
+import data from '../api.json'
 
 const app = document.getElementById('app') // Se trae el elemento donde se va a renderizar
 
@@ -9,5 +9,5 @@ const app = document.getElementById('app') // Se trae el elemento donde se va a 
 // ReactDOM.render(Que se va a renderizar, Donde se va a realizar)
 // const holaMundo = <h1>Hola a todo el mundo</h1>
 render(
-    <Playlist data={data}/>
+    <Home/>
 ,app)
--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,20 +1,23 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Media from './media'
+import Play from '../../icons/components/play'
 import './playlist.css'
 
-class Playlist extends Component {
-    render(){
-        const playlist = this.props.data.categories[0].playlist
-        return (
-            <div className="Playlist">
-                {
-                    playlist.map((item) => {
-                        return <Media {...item} key={item.id} />
-                    })
-                }
-            </div>
-        )
-    }
+function Playlist (props) {
+    const playlist = props.data.categories[0].playlist
+    return (
+        <div className="Playlist">
+            <Play
+                size={50}
+                color="red"
+            />
+            {
+                playlist.map((item) => {
+                    return <Media {...item} key={item.id} />
+                })
+            }
+        </div>
+    )
 }
 
 export default Playlist;
